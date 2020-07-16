b179fbd9049fb8639c1a59d9dbfcdf146344e2c5
Arjun Sreedharan <asreedharan@vmware.com>
2020-07-16 14:49:02 +0000

    Updates version to 0.1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='pip-pop',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/kennethreitz/pip-pop',
     license='MIT',
     author='Kenneth Reitz',
